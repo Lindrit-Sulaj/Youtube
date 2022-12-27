f0f3ddbac75ed35cb4e772bc199b0ce91338a109
Lindrit <lindritsulaj2.0@gmail.com>
2022-12-27 14:08:47 +0100

    Navbar Finished

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,19 +1,26 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import useMediaQuery from '../hooks/useMediaQuery';
+import MenuItem from './MenuItem';
 
 const Navbar = () => {
   const [windowWidth, breakpoint] = useMediaQuery();
   const [menuOpened, setMenuOpened] = useState(false);
+  const [searchTerm, setSearchTerm] = useState('');
+  
+  const handleSearch = () => {
+    if (searchTerm === '' || searchTerm.split(" ").length < 1) return;
+    
+  }
 
   return (
-    <nav className='navbar h-[65px] fixed top-0 w-full tablet:'>
+    <nav className='navbar h-[70px] fixed top-0 w-full pr-4'>
       {(breakpoint === 'sm' || breakpoint === 'md') ? (
         <div className='bg-neutral-200 dark:bg-neutral-850 my-3 mx-4 py-2 px-6 flex items-center rounded-xl relative'>
           <button className='w-[5%] text-black dark:text-blue-400'>
             <i className="fa-solid fa-magnifying-glass"></i>
           </button>
-          <input type="text" className='w-[90%] bg-transparent h-full outline-none px-3 text-white' placeholder='Search for any video' />
+          <input type="text" className='w-[90%] bg-transparent h-full outline-none px-3 text-white' value={searchTerm} onChange={(e) => setSearchTerm(e.target.value)} placeholder='Search for any video' />
           <button className='rounded-full text-blue-500 dark:text-blue-400 text-2xl w-[5%]' onClick={() => setMenuOpened(!menuOpened)}>
             <i className="fa-solid fa-circle-user"></i>
           </button>
@@ -37,23 +44,23 @@ const Navbar = () => {
                 title="History"
                 link="/feed/history" />
               <MenuItem
-                icon="fa-solid fa-clock"
-                title="Watch Later"
-                link="/feed/watch-later" />
+                icon="fa-solid fa-clapperboard"
+                title="Subscriptions"
+                link="/feed/subscriptions" />
             </div>
           )}
         </div>
       ) : (
-        <div className='flex justify-between h-full items-center'>
-          <div className='text-white'>
-            Youtube
-          </div>
+        <div className='flex justify-between h-full items-center px-4'>
           <div>
-            <i className="fa-solid fa-magnifying-glass"></i>
-            <input type="text" />
+            
+          </div>
+          <div className='flex items-center bg-neutral-850 px-4 gap-2 rounded-lg'>
+            <i className="fa-solid fa-magnifying-glass text-white"></i>
+            <input type="text" className='bg-transparent w-[340px] h-12 outline-none text-white px-2' placeholder='Search for videos' value={searchTerm} onChange={(e) => setSearchTerm(e.target.value)}/>
           </div>
 
-          <div>
+          <div className='flex items-center gap-6 justify-end text-2xl text-blue-300'>
             <i className="fa-solid fa-square-plus"></i>
             <i className="fa-solid fa-bell"></i>
             <i className="fa-solid fa-circle-user"></i>
@@ -64,13 +71,6 @@ const Navbar = () => {
   )
 }
 
-const MenuItem = ({ title, link, icon, setMenuOpened }) => {
-  return (
-    <Link to={link} className="transition-all flex px-4 items-center py-2 gap-4 hover:bg-neutral-800" onClick={() => setMenuOpened(false)}>
-      <i className={`${icon} text-neutral-200`}></i>
-      <span className='text-neutral-200'>{title}</span>
-    </Link>
-  )
-}
+
 
 export default Navbar
