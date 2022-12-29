bfaf7a0b09adc818e6939e6943d4a401474c5e3f
Lindrit <lindritsulaj2.0@gmail.com>
2022-12-29 12:59:34 +0100

    Discover created

--- src/pages/Discover.jsx
+++ src/pages/Discover.jsx
@@ -1,24 +1,45 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { suggestedVideos } from '../assets/constants';
+import { VideoCard } from '../components';
+import { useGetSuggestedVideosQuery } from '../redux/services/youtubeApi';
 
 const Discover = () => {
   const [selectedSuggestion, setSelectedSuggestion] = useState(suggestedVideos.all.videoId);
 
+  const { data, isLoading, error } = useGetSuggestedVideosQuery(selectedSuggestion);
+
   return (
     <div className='text-white'>
       <div className='suggestions flex gap-3 px-4 mt-2 overflow-auto'>
-            {
-              Object.entries(suggestedVideos).map((suggestion, id) => (
-                <Suggestion
-                  key={id}
-                  title={suggestion[0]}
-                  videoId={suggestion[1].videoId}
-                  selectedSuggestion={selectedSuggestion}
-                  setSelectedSuggestion={setSelectedSuggestion}
-                />
-              ))
-            }
-          </div>
+        {
+          Object.entries(suggestedVideos).map((suggestion, id) => (
+            <Suggestion
+              key={id}
+              title={suggestion[0]}
+              videoId={suggestion[1].videoId}
+              selectedSuggestion={selectedSuggestion}
+              setSelectedSuggestion={setSelectedSuggestion}
+            />
+          ))
+        }
+      </div>
+
+      {(!isLoading && !error) ? (<div className='videos-normal flex flex-wrap'>
+        {data?.items.map((video, index) => (
+          <VideoCard
+            key={index}
+            videoId={video.id.videoId}
+            channelId={video.snippet.channelId}
+            thumbnail={video.snippet.thumbnails.high.url || video.snippet.thumbnails.default.url}
+            publishTime={video.snippet.publishTime}
+            title={video.snippet.title}
+            channelTitle={video.snippet.channelTitle}
+          />
+        ))}
+
+      </div>) : (
+        <p>Error occurred or is loading</p>
+      )}
     </div>
   )
 }
