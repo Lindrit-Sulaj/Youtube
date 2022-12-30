0e173478e3b1c7fc8dda2c15f0ee49e1ea2aabbc
Lindrit <lindritsulaj2.0@gmail.com>
2022-12-30 13:22:41 +0100

    Channel Videos

--- src/pages/Channel.jsx
+++ src/pages/Channel.jsx
@@ -9,9 +9,9 @@ const Channel = () => {
   const { data, isLoading, error } = useGetChannelDetailsQuery(id);
   const { data: videosData, isLoading: videosLoading, error: videosError } = useGetChannelVideosQuery(id);
 
-  const formatter = Intl.NumberFormat('en', { notation: 'compact' })
+  console.log(videosData);
 
-  console.log(videosData)
+  const formatter = Intl.NumberFormat('en', { notation: 'compact' })
 
   if (isLoading) return <div className='text-white'><Loader /></div>
 
@@ -20,7 +20,7 @@ const Channel = () => {
       {(!isLoading && !error) && (
         <div>
           <div>
-            <img className="w-full h-60 object-cover md:h-80 rounded-xl" src={data.items[0].brandingSettings.image.bannerExternalUrl} alt={`${data.items[0].brandingSettings.channel.title} banner`} />
+            <img className="w-full h-auto object-cover md:h-80 rounded-xl" src={data.items[0].brandingSettings.image?.bannerExternalUrl || 'https://images.unsplash.com/photo-1579548122080-c35fd6820ecb?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80'} alt={`${data.items[0].brandingSettings.channel.title} banner`} />
           </div>
           <div className='flex w-full px-4 md:px-0 mt-8 md:w-5/6 mx-auto gap-4 items-center'>
             <img className="w-24 h-24 object-cover rounded-full" src={data.items[0].snippet.thumbnails.high.url} alt={`${data.items[0].brandingSettings.channel.title} profile picture`} />
@@ -38,12 +38,27 @@ const Channel = () => {
 
           {videosLoading && <p>Loading videos</p>}
           {videosError && <p>Couldn't load videos</p>}
-          { (!videosLoading && !videosError) && (
-            <div className='flex flex-wrap'>
-              { videosData.items.map((video, id) => (
-                <p>Hello</p>
-              )) }
-            </div>  
+          {(!videosLoading && !videosError) && (
+            <div className='px-4 mx-auto mt-14'>
+              <div className='flex gap-6 md:gap-8 px-4'>
+                <p className='text-neutral-300 font-medium font-secondary'>Videos: {formatter.format(data.items[0].statistics.videoCount)}</p>
+                <p className="text-neutral-300 font-medium font-secondary">Views: {formatter.format(data.items[0].statistics.viewCount)}</p>
+              </div>
+              <div className='flex flex-wrap'>
+                {videosData.items.map((video, id) => {
+                  if (!video.id.videoId) return
+                  return <VideoCard
+                    key={id}
+                    thumbnail={video.snippet.thumbnails.high.url || video.snippet.thumbnails.default.url}
+                    title={video.snippet.title}
+                    channelId={video.snippet.channelId}
+                    videoId={video.id.videoId}
+                    publishTime={video.snippet.publishTime}
+                    channelTitle={video.snippet.channelTitle}
+                  />
+                })}
+              </div>
+            </div>
           )}
         </div>
       )}
