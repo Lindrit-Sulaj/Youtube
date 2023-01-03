05eebdb14d58de2dadac86ca9540996e7784849c
Lindrit <lindritsulaj2.0@gmail.com>
2023-01-03 19:31:38 +0100

    ...

--- src/pages/Channel.jsx
+++ src/pages/Channel.jsx
@@ -1,15 +1,48 @@
-import React from 'react';
+import React, { useMemo } from 'react';
 import { useParams } from 'react-router-dom';
+import { useSelector, useDispatch } from 'react-redux';
 
 import { Loader, VideoCard } from '../components';
 import { useGetChannelDetailsQuery, useGetChannelVideosQuery } from '../redux/services/youtubeApi';
+import { subscribe, unsubscribe } from '../redux/features/subsSlice';
 
 const Channel = () => {
   const { id } = useParams();
+  const dispatch = useDispatch();
+  const formatter = Intl.NumberFormat('en', { notation: 'compact' })
   const { data, isLoading, error } = useGetChannelDetailsQuery(id);
   const { data: videosData, isLoading: videosLoading, error: videosError } = useGetChannelVideosQuery(id);
 
-  const formatter = Intl.NumberFormat('en', { notation: 'compact' })
+  const subs = useSelector(state => state.subs);
+  const isSubscribed = useMemo(() => {
+    let index = subs.findIndex(elem => elem.channelId === id);
+    if (index > -1) {
+      return true;
+    } else {
+      return false;
+    }
+  }, [subs, id]);
+
+  const handleSubscribe = () => {
+    let items = data.items;
+
+    if (isSubscribed) {
+      dispatch(unsubscribe(id));
+      return;
+    }
+
+    const channel = {
+      channelId: items[0]?.id,
+      title: items[0]?.brandingSettings?.channel?.title,
+      description: items[0]?.brandingSettings?.channel?.description,
+      customUrl: items[0]?.snippet?.customUrl,
+      thumbnail: items[0]?.snippet?.thumbnails?.high?.url,
+      subscribers: items[0]?.statistics?.subscriberCount,
+      views: items[0]?.statistics?.viewCount
+    }
+
+    dispatch(subscribe(channel));
+  }
 
   if (isLoading) return <div className='text-white'><Loader /></div>
 
@@ -25,12 +58,17 @@ const Channel = () => {
             <img className="w-full h-auto object-cover md:h-80 rounded-xl" src={data.items[0].brandingSettings.image?.bannerExternalUrl || 'https://images.unsplash.com/photo-1579548122080-c35fd6820ecb?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80'} alt={`${data.items[0].brandingSettings.channel.title} banner`} />
           </div>
           <div className='flex w-full px-4 md:px-0 mt-8 md:w-5/6 mx-auto gap-4 items-center'>
-            <img className="w-24 h-24 object-cover rounded-full" src={data.items[0].snippet.thumbnails.high.url} alt={`${data.items[0].brandingSettings.channel.title} profile picture`} />
+            <img className="w-12 h-12 md:w-24 md:h-24 object-cover rounded-full" src={data.items[0].snippet.thumbnails.high.url} alt={`${data.items[0].brandingSettings.channel.title} profile picture`} />
             <div>
-              <h1 className="text-lg font-medium font-secondary">{data?.items[0].brandingSettings.channel.title}</h1>
-              <h2 className='text-neutral-400 mb-2'>{data?.items[0].snippet.customUrl}</h2>
-              <p className='text-neutral-400 font-medium'>{formatter.format(data?.items[0].statistics.subscriberCount)} Subscribers</p>
+              <h1 className="text-[17px] md:text-lg font-medium font-secondary">{data?.items[0].brandingSettings.channel.title}</h1>
+              <h2 className='text-neutral-400 text-[15px] mb-1 md:mb-2 md:text-base'>{data?.items[0].snippet.customUrl}</h2>
+              <p className='text-neutral-400 text-sm font-medium md:text-base'>{formatter.format(data?.items[0].statistics.subscriberCount)} Subscribers</p>
             </div>
+            <button
+              onClick={handleSubscribe}
+              className={`transition-all ml-auto px-4 py-[6px] rounded-md ${!isSubscribed ? 'bg-neutral-600 hover:bg-neutral-700' : 'bg-blue-600 hover:bg-blue-700'}`}>
+              {isSubscribed ? 'Subscribed' : 'Subscribe '}
+            </button>
           </div>
           <div className='w-full md:w-5/6 px-4 md:px-0 mx-auto mt-16'>
             <p className='font-secondary font-medium uppercase text-sky-400'>About</p>
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,13 +1,15 @@
 import { configureStore } from "@reduxjs/toolkit";
 
 import feedReducer from './features/feedSlice';
-import savesReducer from './features/savesSlice'
+import savesReducer from './features/savesSlice';
+import subsReducer from './features/subsSlice';
 import { youtubeApi } from "./services/youtubeApi";
 
 export const store = configureStore({
   reducer: {
     feed: feedReducer,
     saves: savesReducer,
+    subs: subsReducer,
     [youtubeApi.reducerPath]: youtubeApi.reducer,
   },
   middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(youtubeApi.middleware)
