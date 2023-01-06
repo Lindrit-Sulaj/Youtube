c8aa99cd7c8b3b1e4e49ddfc4484d3a9f2d9ec0e
Lindrit <lindritsulaj2.0@gmail.com>
2023-01-06 19:58:51 +0100

    ...

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -7,9 +7,9 @@ const Sidebar = () => {
 
       <div className='flex flex-col items-center gap-3'>
         <MenuItem title="Home" link="/" icon="fa-solid fa-house" />
-        <MenuItem title="Subscriptions" link="/subscriptions" icon="fa-solid fa-clapperboard" />
-        <MenuItem title="Saved videos" link="/saved-videos" icon="fa-solid fa-bookmark" />
-        <MenuItem title="History" link="/history" icon="fa-solid fa-address-book" />
+        <MenuItem title="Subscriptions" link="/feed/subscriptions" icon="fa-solid fa-clapperboard" />
+        <MenuItem title="Saved videos" link="/feed/saved-videos" icon="fa-solid fa-bookmark" />
+        <MenuItem title="History" link="/feed/history" icon="fa-solid fa-address-book" />
         <a href="https://github.com/Lindrit-Sulaj" className="bg-neutral-800 h-14 w-14 flex items-center justify-center rounded-lg">
           <div className='flex justify-center items-center'>
             <i className={`fa-brands fa-github text-neutral-200`}></i>
