24c94ff79c7fa6f6eab2ae99be18c8bc7775ddaa
Lindrit <lindritsulaj2.0@gmail.com>
2023-01-11 14:50:00 +0100

    ...

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import App from './App';
 import './index.css';
 import { Provider } from 'react-redux';
 import { store } from './redux/store';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { HashRouter as Router } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
